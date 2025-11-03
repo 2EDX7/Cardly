bea5a986e1bb92be1b20c5e322d4eb0355abbc8d
akr-sed <abderrehmane.mohamed.akram.seddik@ensia.edu.dz>
2025-11-03 08:23:36 +0100

    feat: implemnted the routing system for the app

---
+++ flutter app/lib/routes/main_wrapper.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../screens/home/home_page.dart';
+import '../screens/addcard/addCard.dart';
+import '../screens/profile/profilePage.dart';
+import '../widgets/navBar.dart';
+
+/// Main wrapper widget that handles bottom navigation between Home, Add Card, and Profile
+class MainWrapper extends StatefulWidget {
+  final int initialIndex;
+  
+  const MainWrapper({
+    Key? key,
+    this.initialIndex = 0,
+  }) : super(key: key);
+
+  @override
+  State<MainWrapper> createState() => _MainWrapperState();
+}
+
+class _MainWrapperState extends State<MainWrapper> {
+  late int _currentIndex;
+  
+  // List of pages to display
+  final List<Widget> _pages = [
+    const HomePage(),
+    const AddCardScreen(),
+    const ProfilePage(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.initialIndex;
+  }
+
+  void _onTabChange(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavBar(
+        activeIndex: _currentIndex,
+        onTabChange: _onTabChange,
+      ),
+    );
+  }
+}
