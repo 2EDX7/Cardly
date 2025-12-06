a65a8db507a3446a5dd166889a92e90c7d0c41ba
akr-sed <abderrehmane.mohamed.akram.seddik@ensia.edu.dz>
2025-12-06 14:01:26 +0100

    feat: setup the user repository for the authentication and signup

---
+++ flutter app/lib/data/repositories/user_repository.dart
@@ -0,0 +1,8 @@
+import '../models/user.dart';
+
+/// Abstraction for user persistence and lookup.
+abstract class UserRepository {
+  Future<User?> getUserByEmail(String email);
+  Future<User> createUser(User user);
+  Future<User?> verifyCredentials({required String email, required String passwordHash});
+}
